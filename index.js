1c1da084903930fcd3cb8312f2276534250c6f0d
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-01 12:57:29 +0530

    Added home

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const hostname = '127.0.0.1'; // Local host ip.
 const port = 3000; // A temporary port.
 const server = http.createServer(function(req, res) {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Home page will come here.\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.sendFile(__dirname + '/src/WebApp/with_radio_final.html');
 });
 server.listen(port, hostname, function() {
   console.log('Server running at http://'+ hostname + ':' + port + '/');
