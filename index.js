b38461f394461ac4c19ec9c6ca1fafd6a26b1248
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-01 13:11:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const http = require('http');
 const hostname = '127.0.0.1'; // Local host ip.
 const port = 3000; // A temporary port.
+const homebase=fs.readFile('./src/WebApp/with_radio_final.htm', function (err, html) {
+    if (err) {
+        throw err; 
+    }
+});
 const server = http.createServer(function(req, res) {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('test');
+  res.setHeader('Content-Type', 'text/html');
+  response.write(homebase);
+  res.end();
 });
 server.listen(port, hostname, function() {
   console.log('Server running at http://'+ hostname + ':' + port + '/');
