e9f68bc75ac88bce83b822320304f10ca7d32714
Sreyas Ravichandran <sreyasravichandran@gmail.com>
2019-07-01 13:05:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000; // A temporary port.
 const server = http.createServer(function(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.send('test');
+  res.end('test');
 });
 server.listen(port, hostname, function() {
   console.log('Server running at http://'+ hostname + ':' + port + '/');
